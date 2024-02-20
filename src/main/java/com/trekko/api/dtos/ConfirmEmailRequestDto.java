0287614c2dddbe615f95f85404eacfd0adbeb90f
Matthias <33065597+matthiaaas@users.noreply.github.com>
2024-02-20 16:12:23 +0100

    feat: add dto constructor

--- src/main/java/com/trekko/api/dtos/ConfirmEmailRequestDto.java
+++ src/main/java/com/trekko/api/dtos/ConfirmEmailRequestDto.java
@@ -3,6 +3,13 @@ package com.trekko.api.dtos;
 public class ConfirmEmailRequestDto {
     private String code;
 
+    public ConfirmEmailRequestDto() {
+    }
+
+    public ConfirmEmailRequestDto(final String code) {
+        this.code = code;
+    }
+
     public String getCode() {
         return this.code;
     }

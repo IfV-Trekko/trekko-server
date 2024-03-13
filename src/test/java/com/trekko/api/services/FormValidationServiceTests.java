bb8383a4318e934dd5a5f888ba688318ad9e11dd
Matthias <33065597+matthiaaas@users.noreply.github.com>
2024-03-13 19:31:42 +0100

    test: add more test cases for form validation

--- src/test/java/com/trekko/api/services/FormValidationServiceTests.java
+++ src/test/java/com/trekko/api/services/FormValidationServiceTests.java
@@ -7,145 +7,206 @@ import org.junit.jupiter.api.Test;
 
 public class FormValidationServiceTests {
 
-    private FormValidationService formValidationService;
+        private FormValidationService formValidationService;
 
-    @BeforeEach
-    public void setUp() {
-        this.formValidationService = new FormValidationService();
-    }
+        @BeforeEach
+        public void setUp() {
+                this.formValidationService = new FormValidationService();
+        }
 
-    @Test
-    public void testValidFormSubmission() {
-        String formTemplateJson = "{\n" + //
-                "    \"fields\": [\n" + //
-                "        {\n" + //
-                "            \"title\": \"Alter\",\n" + //
-                "            \"key\": \"age\",\n" + //
-                "            \"type\": \"number\"\n" + //
-                "        }\n" + //
-                "    ]\n" + //
-                "}";
-        String submittedDataJson = "{\n" + //
-                "    \"age\": 12\n" + //
-                "}";
-        assertTrue(formValidationService.validateFormData(formTemplateJson, submittedDataJson));
-    }
+        @Test
+        public void testValidFormSubmission() {
+                String formTemplateJson = "{\n" + //
+                                "    \"fields\": [\n" + //
+                                "        {\n" + //
+                                "            \"title\": \"Alter\",\n" + //
+                                "            \"key\": \"age\",\n" + //
+                                "            \"type\": \"number\"\n" + //
+                                "        }\n" + //
+                                "    ]\n" + //
+                                "}";
+                String submittedDataJson = "{\n" + //
+                                "    \"age\": 12\n" + //
+                                "}";
+                assertTrue(formValidationService.validateFormData(formTemplateJson, submittedDataJson));
+        }
 
-    @Test
-    public void testInvalidFormSubmissionWithInvalidKeys() {
-        String formTemplateJson = "{\n" + //
-                "    \"fields\": [\n" + //
-                "        {\n" + //
-                "            \"title\": \"Alter\",\n" + //
-                "            \"key\": \"age\",\n" + //
-                "            \"type\": \"number\"\n" + //
-                "        }\n" + //
-                "    ]\n" + //
-                "}";
-        String submittedDataJson = "{\n" + //
-                "    \"age\": 12,\n" + //
-                "    \"name\": \"Max\"\n" + //
-                "}";
-        assertFalse(formValidationService.validateFormData(formTemplateJson, submittedDataJson));
-    }
+        @Test
+        public void testInvalidFormSubmissionWithInvalidKeys() {
+                String formTemplateJson = "{\n" + //
+                                "    \"fields\": [\n" + //
+                                "        {\n" + //
+                                "            \"title\": \"Alter\",\n" + //
+                                "            \"key\": \"age\",\n" + //
+                                "            \"type\": \"number\"\n" + //
+                                "        }\n" + //
+                                "    ]\n" + //
+                                "}";
+                String submittedDataJson = "{\n" + //
+                                "    \"age\": 12,\n" + //
+                                "    \"name\": \"Max\"\n" + //
+                                "}";
+                assertFalse(formValidationService.validateFormData(formTemplateJson, submittedDataJson));
+        }
 
-    @Test
-    public void testInvalidFormSubmissionWithMissingKeys() {
-        String formTemplateJson = "{\n" + //
-                "    \"fields\": [\n" + //
-                "        {\n" + //
-                "            \"title\": \"Alter\",\n" + //
-                "            \"key\": \"age\",\n" + //
-                "            \"type\": \"number\"\n" + //
-                "        },\n" + //
-                "        {\n" + //
-                "            \"title\": \"Name\",\n" + //
-                "            \"key\": \"name\",\n" + //
-                "type\": \"text\"\n" + //
-                "        }\n" + //
-                "    ]\n" + //
-                "}";
-        String submittedDataJson = "{\n" + //
-                "    \"age\": 12\n" + //
-                "}";
-        assertFalse(formValidationService.validateFormData(formTemplateJson, submittedDataJson));
-    }
+        @Test
+        public void testInvalidFormSubmissionWithMissingKeys() {
+                String formTemplateJson = "{\n" + //
+                                "    \"fields\": [\n" + //
+                                "        {\n" + //
+                                "            \"title\": \"Alter\",\n" + //
+                                "            \"key\": \"age\",\n" + //
+                                "            \"type\": \"number\"\n" + //
+                                "        },\n" + //
+                                "        {\n" + //
+                                "            \"title\": \"Name\",\n" + //
+                                "            \"key\": \"name\",\n" + //
+                                "type\": \"text\"\n" + //
+                                "        }\n" + //
+                                "    ]\n" + //
+                                "}";
+                String submittedDataJson = "{\n" + //
+                                "    \"age\": 12\n" + //
+                                "}";
+                assertFalse(formValidationService.validateFormData(formTemplateJson, submittedDataJson));
+        }
 
-    @Test
-    public void testValidFormSubmissionWithMissingKeysAndStrictFalse() {
-        String formTemplateJson = "{\n" + //
-                "    \"fields\": [\n" + //
-                "        {\n" + //
-                "            \"title\": \"Alter\",\n" + //
-                "            \"key\": \"age\",\n" + //
-                "            \"type\": \"number\"\n" + //
-                "        },\n" + //
-                "        {\n" + //
-                "            \"title\": \"Name\",\n" + //
-                "            \"key\": \"name\",\n" + //
-                "            \"type\": \"text\"\n" + //
-                "        }\n" + //
-                "    ]\n" + //
-                "}";
-        String submittedDataJson = "{\n" + //
-                "    \"age\": 12\n" + //
-                "}";
-        assertTrue(formValidationService.validateFormData(formTemplateJson, submittedDataJson, false));
-    }
+        @Test
+        public void testValidFormSubmissionWithMissingKeysAndStrictFalse() {
+                String formTemplateJson = "{\n" + //
+                                "    \"fields\": [\n" + //
+                                "        {\n" + //
+                                "            \"title\": \"Alter\",\n" + //
+                                "            \"key\": \"age\",\n" + //
+                                "            \"type\": \"number\"\n" + //
+                                "        },\n" + //
+                                "        {\n" + //
+                                "            \"title\": \"Name\",\n" + //
+                                "            \"key\": \"name\",\n" + //
+                                "            \"type\": \"text\"\n" + //
+                                "        }\n" + //
+                                "    ]\n" + //
+                                "}";
+                String submittedDataJson = "{\n" + //
+                                "    \"age\": 12\n" + //
+                                "}";
+                assertTrue(formValidationService.validateFormData(formTemplateJson, submittedDataJson, false));
+        }
 
-    @Test
-    public void testInvalidFormSubmissionWithInvalidTypes() {
-        String formTemplateJson = "{\n" + //
-                "    \"fields\": [\n" + //
-                "        {\n" + //
-                "            \"title\": \"Alter\",\n" + //
-                "            \"key\": \"age\",\n" + //
-                "            \"type\": \"number\"\n" + //
-                "        }\n" + //
-                "    ]\n" + //
-                "}";
-        String submittedDataJson = "{\n" + //
-                "    \"age\": \"12\"\n" + //
-                "}";
-        assertFalse(formValidationService.validateFormData(formTemplateJson, submittedDataJson));
-    }
+        @Test
+        public void testInvalidFormSubmissionWithInvalidTypes() {
+                String formTemplateJson = "{\n" + //
+                                "    \"fields\": [\n" + //
+                                "        {\n" + //
+                                "            \"title\": \"Alter\",\n" + //
+                                "            \"key\": \"age\",\n" + //
+                                "            \"type\": \"number\"\n" + //
+                                "        }\n" + //
+                                "    ]\n" + //
+                                "}";
+                String submittedDataJson = "{\n" + //
+                                "    \"age\": \"12\"\n" + //
+                                "}";
+                assertFalse(formValidationService.validateFormData(formTemplateJson, submittedDataJson));
+        }
 
-    @Test
-    public void testInvalidFormSubmissionWithMissingRequiredField() {
-        String formTemplateJson = "{\n" + //
-                "    \"fields\": [\n" + //
-                "        {\n" + //
-                "            \"title\": \"Alter\",\n" + //
-                "            \"key\": \"age\",\n" + //
-                "            \"type\": \"number\",\n" + //
-                "            \"required\": true\n" + //
-                "        }\n" + //
-                "    ]\n" + //
-                "}";
-        String submittedDataJson = "{}";
-        assertFalse(formValidationService.validateFormData(formTemplateJson, submittedDataJson));
-    }
+        @Test
+        public void testInvalidFormSubmissionWithMissingRequiredField() {
+                String formTemplateJson = "{\n" + //
+                                "    \"fields\": [\n" + //
+                                "        {\n" + //
+                                "            \"title\": \"Alter\",\n" + //
+                                "            \"key\": \"age\",\n" + //
+                                "            \"type\": \"number\",\n" + //
+                                "            \"required\": true\n" + //
+                                "        }\n" + //
+                                "    ]\n" + //
+                                "}";
+                String submittedDataJson = "{}";
+                assertFalse(formValidationService.validateFormData(formTemplateJson, submittedDataJson));
+        }
 
-    @Test
-    public void testValidFormSubmissionWithMultipleFields() {
-        String formTemplateJson = "{\n" + //
-                "    \"fields\": [\n" + //
-                "        {\n" + //
-                "            \"title\": \"Alter\",\n" + //
-                "            \"key\": \"age\",\n" + //
-                "            \"type\": \"number\"\n" + //
-                "        },\n" + //
-                "        {\n" + //
-                "            \"title\": \"Name\",\n" + //
-                "            \"key\": \"name\",\n" + //
-                "            \"type\": \"text\"\n" + //
-                "        }\n" + //
-                "    ]\n" + //
-                "}";
-        String submittedDataJson = "{\n" + //
-                "    \"age\": 12,\n" + //
-                "    \"name\": \"Max\"\n" + //
-                "}";
-        assertTrue(formValidationService.validateFormData(formTemplateJson, submittedDataJson));
-    }
+        @Test
+        public void testValidFormSubmissionWithMultipleFields() {
+                String formTemplateJson = "{\n" + //
+                                "    \"fields\": [\n" + //
+                                "        {\n" + //
+                                "            \"title\": \"Alter\",\n" + //
+                                "            \"key\": \"age\",\n" + //
+                                "            \"type\": \"number\"\n" + //
+                                "        },\n" + //
+                                "        {\n" + //
+                                "            \"title\": \"Name\",\n" + //
+                                "            \"key\": \"name\",\n" + //
+                                "            \"type\": \"text\"\n" + //
+                                "        }\n" + //
+                                "    ]\n" + //
+                                "}";
+                String submittedDataJson = "{\n" + //
+                                "    \"age\": 12,\n" + //
+                                "    \"name\": \"Max\"\n" + //
+                                "}";
+                assertTrue(formValidationService.validateFormData(formTemplateJson, submittedDataJson));
+        }
+
+        @Test
+        public void testInvalidFormSubmissionWithMultipleFields() {
+                String formTemplateJson = "{\n" + //
+                                "    \"fields\": [\n" + //
+                                "        {\n" + //
+                                "            \"title\": \"Alter\",\n" + //
+                                "            \"key\": \"age\",\n" + //
+                                "            \"type\": \"number\"\n" + //
+                                "        },\n" + //
+                                "        {\n" + //
+                                "            \"title\": \"Name\",\n" + //
+                                "            \"key\": \"name\",\n" + //
+                                "            \"type\": \"text\"\n" + //
+                                "        }\n" + //
+                                "    ]\n" + //
+                                "}";
+                String submittedDataJson = "{\n" + //
+                                "    \"age\": \"Invalid Age\"\n" + //
+                                "    \"name\": \"Max\"\n" + //
+                                "}";
+                assertFalse(formValidationService.validateFormData(formTemplateJson, submittedDataJson));
+        }
+
+        @Test
+        public void testValidFormSubmissionWithMultipleFieldsAndStrictFalse() {
+                String formTemplateJson = "{\n" + //
+                                "    \"fields\": [\n" + //
+                                "        {\n" + //
+                                "            \"title\": \"Alter\",\n" + //
+                                "            \"key\": \"age\",\n" + //
+                                "            \"type\": \"number\"\n" + //
+                                "        },\n" + //
+                                "        {\n" + //
+                                "            \"title\": \"Name\",\n" + //
+                                "            \"key\": \"name\",\n" + //
+                                "            \"type\": \"text\"\n" + //
+                                "        }\n" + //
+                                "    ]\n" + //
+                                "}";
+                String submittedDataJson = "{\n" + //
+                                "    \"age\": 12,\n" + //
+                                "    \"name\": \"Max\"\n" + //
+                                "}";
+                assertTrue(formValidationService.validateFormData(formTemplateJson, submittedDataJson, false));
+        }
+
+        @Test
+        public void testSubmissionWithInvalidJson() {
+                String formTemplateJson = "{\n" + //
+                                "    \"fields\": [\n" + //
+                                "        {\n" + //
+                                "            \"title\": \"Alter\",\n" + //
+                                "            \"key\": \"age\",\n" + //
+                                "            \"type\": \"number\"\n" + //
+                                "        }\n" + //
+                                "    ]\n" + //
+                                "}";
+                String submittedDataJson = "Invalid{{{";
+                assertFalse(formValidationService.validateFormData(formTemplateJson, submittedDataJson));
+        }
 }

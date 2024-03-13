808bc96aa8f9b4f84912f41279604be95e8e5f33
Matthias <33065597+matthiaaas@users.noreply.github.com>
2024-03-13 18:05:48 +0100

    test: add onboarding controller test

---
+++ src/test/java/com/trekko/api/controllers/OnboardingControllerTests.java
@@ -0,0 +1,70 @@
+package com.trekko.api.controllers;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.trekko.api.interceptors.JwtAuthFilter;
+import com.trekko.api.models.User;
+import com.trekko.api.repositories.UserRepository;
+import com.trekko.api.utils.CustomUserDetails;
+
+@WebMvcTest(OnboardingController.class)
+public class OnboardingControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private WebApplicationContext context;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @Mock
+    private CustomUserDetails customUserDetails;
+    @Mock
+    private User user;
+
+    @BeforeEach
+    public void setupAuthentication() {
+        this.mockMvc = MockMvcBuilders
+                .webAppContextSetup(this.context)
+                .addFilters(new JwtAuthFilter(this.userRepository))
+                .build();
+
+        when(this.customUserDetails.getUser()).thenReturn(user);
+
+        final UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                customUserDetails, null, customUserDetails.getAuthorities());
+
+        final SecurityContext securityContext = mock(SecurityContext.class);
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        SecurityContextHolder.setContext(securityContext);
+    }
+
+    @Test
+    public void getAboutText() throws Exception {
+        this.mockMvc.perform(get("/onboarding/texts/about"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getGoalText() throws Exception {
+        this.mockMvc.perform(get("/onboarding/texts/goal"))
+                .andExpect(status().isOk());
+    }
+}
